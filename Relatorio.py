179e60c3344fca813f54549b33b1ef32ef2cd376
Gabriel <gabrielmorais.12@hotmail.com>
2026-09-24 10:26:29 -0300

    Corrigindo erro do CSV

--- Relatorio.py
+++ Relatorio.py
@@ -1575,6 +1575,10 @@ def tela_checkup():
                             if reb1.lower() in ['nan', 'none', '']: reb1 = "-"
                             if reb2.lower() in ['nan', 'none', '']: reb2 = "-"
 
+                    # --- NOVA REGRA: Se tem rebolo e não achou na planilha, exibe "Não cadastrado" ---
+                    if tem_rebolo == "SIM" and reb1 == "-":
+                        reb1 = "Não cadastrado"
+
                     # Lógica de Cores
                     if "PARADA" in st_m.upper():
                         bg_color, bd_color = "#FECACA", "#F87171" # Vermelho
@@ -1599,7 +1603,7 @@ def tela_checkup():
 <div style="margin: 0; font-size: 13px; color: #000000 !important;"><b>🛞 Reb 2:</b> {reb2}</div>
                         '''
 
-                    # Construção do HTML do Post-it (Trocado <h4> por <div> para não sofrer overwrite do CSS global)
+                    # Construção do HTML do Post-it
                     html = f'''<div style="background-color: {bg_color}; padding: 15px; border-radius: 2px 20px 2px 15px; box-shadow: 3px 5px 10px rgba(0,0,0,0.4); color: #000000 !important; margin-bottom: 20px; min-height: 200px; transform: rotate({rotate}deg);">
 <div style="margin: 0 0 10px 0; color: #000000 !important; border-bottom: 1px solid {bd_color}; font-size: 16px; font-weight: bold; padding-bottom: 5px;">⚙️ {setor_m} {maq_m}</div>
 <div style="margin: 0 0 4px 0; font-size: 14px; color: #000000 !important;"><b>⏰ Agendado para:</b> {h_alvo}</div>
